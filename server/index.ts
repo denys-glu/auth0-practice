8be9815a4315c17e35bffc940487324106171266
denys_glu <denis.glukhovskyy@gmail.com>
2021-03-09 12:31:26 -0800

    small cleanup

--- server/index.ts
+++ server/index.ts
@@ -8,12 +8,12 @@ import { ticketRoutes, projectRoutes, userRoutes } from "./routes";
 
 const app = express();
 
-// This will fire our mongoose.connect statement to initialize our database connection
 connectToDB();
 
 app.use(cors(), express.urlencoded({ extended: true }), express.json());
 
-// This is where we import the users routes function from our user.routes.js file
 app.use(ticketRoutes, projectRoutes, userRoutes)
 
 const server = app.listen(PORT, () => console.log(`The server is all fired up on port ${PORT}`));
+
+export default server;
